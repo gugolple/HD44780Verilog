b0a64150b7e445f3372ddc7cf6b91596b7f3dd6e
gugolple <juanignacio.croce@gmail.com>
2025-04-26 17:05:21 +0200

    Tested in verilator project
    Now full reset/initialization sequence is complete

--- src/hd44780.v
+++ src/hd44780.v
@@ -1,3 +1,10 @@
+`define EXPECTED_FREQ 250_000 // 250khz
+`define MILLI 1_000
+`define MICRO 1_000_000
+`define MILLI_TO_CLOCK(t) (t*`EXPECTED_FREQ/`MILLI)
+`define MICRO_TO_CLOCK(t) (t*`EXPECTED_FREQ/`MICRO)
+`define POWERON_DELAY_MILLI 100
+`define CLEAR_SCREEN_DELAY_MILLI 10
 //8 bit mode
 `define BUS_WIDTH 4
 module hd44780 
@@ -5,7 +12,7 @@ module hd44780
   input clk,
   input rst,
   input trg,
-  output busy,
+  output reg busy,
   output reg e,
   output reg rs,
   output reg [`BUS_WIDTH-1:0] db
@@ -29,6 +36,34 @@ localparam [`INST_WIDTH-1:0] SDL2 = `INST_WIDTH'hC0; // 0x80 + 0x40
 localparam [`INST_WIDTH-1:0] SDL3 = `INST_WIDTH'h90; // 0x80 + 0x10
 localparam [`INST_WIDTH-1:0] SDL4 = `INST_WIDTH'hD0; // 0x80 + 0x50
 
+
+localparam [`INST_WIDTH-1:0]INST_DISPLAY_CLEAR   = 8'b00000001;
+localparam [`INST_WIDTH-1:0]INST_RETURN_HOME     = 8'b00000010;
+`define CURSOR_DIRECTION 1 // 0 - left | 1 - right
+`define SHIFT_CURSOR 1 // 0 - off | 1 - on
+localparam [`INST_WIDTH-1:0]INST_ENTRY_MODE      = 8'b000001`CURSOR_DIRECTION`SHIFT_CURSOR;
+`define DISPLAY_ON_OFF 1 // 0 - off | 1 - on
+`define CURSOR_ON_OFF 1 // 0 - off | 1 - on
+`define CURSOR_BLINK 0 // 0 - off | 1 - on
+localparam [`INST_WIDTH-1:0]INST_DISPLAY_CONTROL = 8'b00001`DISPLAY_ON_OFF`CURSOR_ON_OFF`CURSOR_BLINK;
+// SC - RL Table:
+//  0    0 Shift cursor to the left (AC-1)
+//  0    1 Shift cursor to the right (AC+1)
+//  1    0 Shift display to the left, cursor follows
+//  1    1 Shift display to the right, cursor follows
+`define DISPLAY_SHIFT_SC 0
+`define DISPLAY_SHIFT_RL 0
+`define DISPLAY_PAD 00
+localparam [`INST_WIDTH-1:0]INST_DISPLAY_SHIFT   = 8'b0001`DISPLAY_SHIFT_SC`DISPLAY_SHIFT_RL`DISPLAY_PAD;
+// Hardware configuration of unit
+`define DATA_LENGTH    0 //0 - 4 | 1 - 8 //Bits for comm
+`define DISPLAY_LINES  1 //0 - 1 | 1 - 2
+`define CHARACTER_FONT 0 
+`define FUNCTION_SET_PAD 00
+localparam [`INST_WIDTH-1:0]INST_FUNCTION_SET    = 8'b001`DATA_LENGTH`DISPLAY_LINES`CHARACTER_FONT`FUNCTION_SET_PAD;
+localparam [`INST_WIDTH-1:0]INST_SET_CGRAM_ADDR  = 8'b01000000;
+localparam [`INST_WIDTH-1:0]INST_SET_DDRAM_ADDR  = 8'b10000000;
+
 // Data memory definitions
 `define DATAMEMWIDTH 8
 `define DATAMEMDEPTH 16
@@ -48,72 +83,12 @@ assign sdl =
   | (SDL3 & {`INST_WIDTH{datalsel == 2}})
   | (SDL4 & {`INST_WIDTH{datalsel == 3}});
 
+reg [`DATAMEMADRW-1:0] counterdataramaddress;
 assign dataldata =
   (datal1[counterdataramaddress] & {`DATAMEMWIDTH{datalsel == 0}})
   | (datal2[counterdataramaddress] & {`DATAMEMWIDTH{datalsel == 1}})
   | (datal3[counterdataramaddress] & {`DATAMEMWIDTH{datalsel == 2}})
   | (datal4[counterdataramaddress] & {`DATAMEMWIDTH{datalsel == 3}});
-
-// Clock selector
-`define COUNTERSELECTORBITS 2
-reg [`COUNTERSELECTORBITS-1:0] demuxclkval;
-wire [2**`COUNTERSELECTORBITS-1:0] demuxclksel;
-demux #(
-    .BITS(`COUNTERSELECTORBITS)
-  ) demuxclk (
-    .val(demuxclkval),
-    .sel(demuxclksel)
-  );
-
-// Clock dividers for the waits
-reg iact;
-wire irst;
-assign irst = rst | iact;
-wire waitclk;
-//Enable activation
-wire counterwaitinstenable;
-counter #(
-    .COUNT(20)
-  )counterwaitinste (
-    .clk(clk),
-    .rst(rst),
-    .flag(counterwaitinstenable)
-  );
-// Basic instruction process delay
-wire counterwaitinstprocess;
-counter #(
-    .COUNT(20)
-  )counterwaitinstp (
-    .clk(clk),
-    .rst(rst),
-    .flag(counterwaitinstprocess)
-  );
-// 10ms wait
-wire counterwait10ms;
-counter #(
-    .COUNT(2500)
-  )counterwait10m (
-    .clk(clk),
-    .rst(rst),
-    .flag(counterwait10ms)
-  );
-// 200ms wait
-wire counterwait200ms;
-counter #(
-    .COUNT(50000)
-  )counterwait100m (
-    .clk(clk),
-    .rst(rst),
-    .flag(counterwait200ms)
-  );
-
-assign waitclk = 
-  (counterwaitinstenable & demuxclksel[0]) 
-  | (counterwaitinstprocess & demuxclksel[1])
-  | (counterwait10ms & demuxclksel[2])
-  | (counterwait200ms & demuxclksel[3]);
-
-assign busy = trigger;
 
 // Initialization
 initial begin
@@ -123,314 +98,121 @@ initial begin
   $readmemh("hd44780datal4.mem", datal4);
 end
 
-`define COUNTERSTATEW 4
-reg [`COUNTERSTATEW-1:0] counterstate;
-reg [`DATAMEMADRW-1:0] counterdataramaddress;
-reg trigger;
-reg rdy;
-reg clks;
+`define TIMECOUNTERWIDHT 32
+reg [`TIMECOUNTERWIDHT-1:0]timecounter;
 always @(posedge clk, negedge rst) begin
   if (!rst) begin
-    trigger <= 1'b1;
-    counterstate <= {`COUNTERSTATEW {1'b0}};
-    demuxclkval <= 3; //200ms, restart wait
-    iact <= 1'b0;
+    busy <= 1'b1;
     rs <= 1'b0;
     e <= 1'b0;
     db <= {`BUS_WIDTH {1'b0}};
-    demuxclkval <= 3;
-    rdy <= 1'b0;
     datalsel <= 0;
+    timecounter <= {`TIMECOUNTERWIDHT {1'b0}}; // Set to 0
   end else begin
-    if (!rdy) begin
-      // wait resstart
-      if (counterstate == 0) begin
-        if (waitclk) begin
-          // Once wait is reached, set to Function Set, first only half
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= FS[7:4];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+    timecounter <= timecounter + 1;
+    case (timecounter)
+`define TIME_START 100
+// Wait 100 millis, send function set
+`define FUNCTION_SET_1_HIGH (`TIME_START + `MILLI_TO_CLOCK(`POWERON_DELAY_MILLI))
+        `FUNCTION_SET_1_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_FUNCTION_SET[7:4];
         end
-      end else if (counterstate == 1) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 2; // Wait for 10ms
-          counterstate <= counterstate + 1;
+`define FUNCTION_SET_1_LOW (`FUNCTION_SET_1_HIGH + 1)
+        `FUNCTION_SET_1_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 2) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Function Set, this time send the two
-          // halfs, this is only top
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= FS[7:4];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+// Wait 10 millis, send function set high part
+`define FUNCTION_SET_2_H_HIGH (`FUNCTION_SET_1_LOW + `MILLI_TO_CLOCK(`CLEAR_SCREEN_DELAY_MILLI) + 1)
+        `FUNCTION_SET_2_H_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_FUNCTION_SET[7:4];
         end
-      end else if (counterstate == 3) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 0;
-          counterstate <= counterstate + 1;
+`define FUNCTION_SET_2_H_LOW (`FUNCTION_SET_2_H_HIGH + 1)
+        `FUNCTION_SET_2_H_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 4) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Function Set, this time send the two
-          // halfs, this is only bottom
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= FS[3:0];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+`define FUNCTION_SET_2_L_HIGH (`FUNCTION_SET_2_H_LOW + 1)
+        `FUNCTION_SET_2_L_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_FUNCTION_SET[3:0];
         end
-      end else if (counterstate == 5) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 3; // Wait for 10ms
-          counterstate <= counterstate + 1;
+`define FUNCTION_SET_2_L_LOW (`FUNCTION_SET_2_L_HIGH + 1)
+        `FUNCTION_SET_2_L_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 6) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Display Control, this time send the two
-          // halfs, this is only top
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= DC[7:4];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+// Wait 10 millis, send function set high part
+`define DISPLAY_CLEAR_H_HIGH (`FUNCTION_SET_2_L_LOW + `MILLI_TO_CLOCK(`CLEAR_SCREEN_DELAY_MILLI) + 1)
+        `DISPLAY_CLEAR_H_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_DISPLAY_CLEAR[7:4];
         end
-      end else if (counterstate == 7) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 0;
-          counterstate <= counterstate + 1;
+`define DISPLAY_CLEAR_H_LOW (`DISPLAY_CLEAR_H_HIGH + 1)
+        `DISPLAY_CLEAR_H_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 8) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Display Control, this time send the two
-          // halfs, this is only bottom
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= DC[3:0];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+`define DISPLAY_CLEAR_L_HIGH (`DISPLAY_CLEAR_H_LOW + 1)
+        `DISPLAY_CLEAR_L_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_DISPLAY_CLEAR[3:0];
         end
-      end else if (counterstate == 9) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 1; // Wait 40 us
-          counterstate <= counterstate + 1;
+`define DISPLAY_CLEAR_L_LOW (`DISPLAY_CLEAR_L_HIGH + 1)
+        `DISPLAY_CLEAR_L_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 10) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Entry Mode, this time send the two
-          // halfs, this is only top
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= EM[7:4];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+// Wait 10 millis, send function set high part
+`define DISPLAY_CONTROL_H_HIGH (`DISPLAY_CLEAR_L_LOW + `MILLI_TO_CLOCK(`CLEAR_SCREEN_DELAY_MILLI) + 1)
+        `DISPLAY_CONTROL_H_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_DISPLAY_CONTROL[7:4];
         end
-      end else if (counterstate == 11) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 0;
-          counterstate <= counterstate + 1;
+`define DISPLAY_CONTROL_H_LOW (`DISPLAY_CONTROL_H_HIGH + 1)
+        `DISPLAY_CONTROL_H_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 12) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set to Entry Mode, this time send the two
-          // halfs, this is only bottom
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b1;
-          rs <= 1'b0;
-          db <= EM[3:0];
-          demuxclkval <= 0;
-          iact <= 1'b1;
-          counterstate <= counterstate + 1;
+`define DISPLAY_CONTROL_L_HIGH (`DISPLAY_CONTROL_H_LOW + 1)
+        `DISPLAY_CONTROL_L_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_DISPLAY_CONTROL[3:0];
         end
-      end else if (counterstate == 13) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          e <= 1'b0;
-          demuxclkval <= 1; // Wait 40 us
-          counterstate <= counterstate + 1;
+`define DISPLAY_CONTROL_L_LOW (`DISPLAY_CONTROL_L_HIGH + 1)
+        `DISPLAY_CONTROL_L_LOW: begin
+            e <= 1'b0;
         end
-      end else if (counterstate == 14) begin
-        if (iact) begin
-          iact <= 1'b0;
-        end else if (waitclk) begin
-          // Once wait is reached, set enable to 0, commiting instruction
-          iact <= 1'b1; // Set reset for counters
-          rdy <= 1'b1; // Finalized startup sequence
-          counterstate <= 0; // Reset counter
+// Wait 10 millis, send function set high part
+`define ENTRY_MODE_H_HIGH (`DISPLAY_CONTROL_L_LOW + `MILLI_TO_CLOCK(`CLEAR_SCREEN_DELAY_MILLI) + 1)
+        `ENTRY_MODE_H_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_ENTRY_MODE[7:4];
         end
-      end
-    end else begin
-      // Capture the trigger request
-      if (trg) begin
-        trigger <= 1'b1;
-      end
-      if (trigger) begin
-        if (datalsel != 4) begin
-          if (counterstate == 0) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              // Once wait is reached, set to Set DDRam Address, this time send the two
-              // halfs, this is only top
-              iact <= 1'b1; // Set reset for counters
-              counterdataramaddress <= 0;
-              e <= 1'b1;
-              rs <= 1'b0;
-              db <= sdl[7:4];
-              demuxclkval <= 0;
-              iact <= 1'b1;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 1) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              // Once wait is reached, set enable to 0, commiting instruction
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b0;
-              demuxclkval <= 0;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 2) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              // Once wait is reached, set to Set DDRam Address, this time send the two
-              // halfs, this is only bottom
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b1;
-              rs <= 1'b0;
-              db <= sdl[3:0];
-              demuxclkval <= 0;
-              iact <= 1'b1;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 3) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              // Once wait is reached, set enable to 0, commiting instruction
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b0;
-              demuxclkval <= 1;
-              counterstate <= counterstate + 1;
-            end
-          // Configured for printing, restarted at address 0
-          // Now loop 16 times
-          end else if (counterstate == 4) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b1;
-              rs <= 1'b1;
-              db <= dataldata[7:4];
-              demuxclkval <= 0;
-              iact <= 1'b1;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 5) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b0;
-              demuxclkval <= 1;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 6) begin
-            if (iact) begin
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b1;
-              rs <= 1'b1;
-              db <= dataldata[3:0];
-              demuxclkval <= 0;
-              iact <= 1'b1;
-              counterstate <= counterstate + 1;
-            end
-          end else if (counterstate == 7) begin
-            if (iact) begin
-              counterdataramaddress <= counterdataramaddress + 1;
-              iact <= 1'b0;
-            end else if (waitclk) begin
-              iact <= 1'b1; // Set reset for counters
-              e <= 1'b0;
-              demuxclkval <= 1;
-              counterstate <= 4;
-              if (counterdataramaddress == `DATAMEMDEPTHMAX) begin
-                counterstate <= 0;
-                datalsel <= datalsel + 1;
-              end
-            end
-          end
-        end else begin
-          trigger <= 0;
-          datalsel <= 0;
+`define ENTRY_MODE_H_LOW (`ENTRY_MODE_H_HIGH + 1)
+        `ENTRY_MODE_H_LOW: begin
+            e <= 1'b0;
         end
-      end
-    end
+`define ENTRY_MODE_L_HIGH (`ENTRY_MODE_H_LOW + 1)
+        `ENTRY_MODE_L_HIGH: begin
+            e <= 1'b1;
+            rs <= 1'b0;
+            db <= INST_ENTRY_MODE[3:0];
+        end
+`define ENTRY_MODE_L_LOW (`ENTRY_MODE_L_HIGH + 1)
+        `ENTRY_MODE_L_LOW: begin
+            e <= 1'b0;
+        end
+// Wait 10 millis, finalize all
+`define RESET_CLEAR (`ENTRY_MODE_L_LOW + `MILLI_TO_CLOCK(`CLEAR_SCREEN_DELAY_MILLI) + 1)
+        `RESET_CLEAR: begin
+            busy <= 1'b0;
+        end
+    endcase
   end
 end
 
